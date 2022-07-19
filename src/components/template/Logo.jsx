22d03c363827eae6be6e041570c6a9d0c068828e
AltoGiroLab <dev1@altogiro.net>
2022-07-19 16:11:38 -0300

    navbar e logo ajustados

--- src/components/template/Logo.jsx
+++ src/components/template/Logo.jsx
@@ -1,7 +1,11 @@
-import './Logo.css'
-import React from 'react'
+import "./Logo.css";
+import logo from "../../assets/img/logo.svg";
+import React from "react";
 
-export default props =>
-<aside className="logo">
-    Logo
-</aside>
+export default (props) => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
