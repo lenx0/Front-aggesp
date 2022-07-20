b0fd83c38a5f3753dba191cbedcbda093945a075
AltoGiroLab <dev1@altogiro.net>
2022-07-20 13:17:41 -0300

    crud completo

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -17,6 +17,12 @@ const initialState = {
 export default class UserCrud extends Component {
   state = { ...initialState };
 
+  componentWillMount() {
+    axios(baseUrl).then((resp) => {
+      this.setState({ list: resp.data });
+    });
+  }
+
   clear() {
     this.setState({ user: initialState.user });
   }
@@ -31,9 +37,9 @@ export default class UserCrud extends Component {
     });
   }
 
-  getUpdatedList(user) {
+  getUpdatedList(user, add = true) {
     const list = this.state.list.filter((u) => u.id !== user.id);
-    list.unshift(user);
+    if (add) list.unshift(user);
     return list;
   }
 
@@ -77,23 +83,77 @@ export default class UserCrud extends Component {
         <hr />
         <div className="row">
           <div className="col-12 d-flex justify-content-end">
-            <button className="btn btn-primary" 
-                onClick={(e) => this.save(e)}>
-                Salvar
+            <button className="btn btn-primary" onClick={(e) => this.save(e)}>
+              Salvar
             </button>
-
-            <button className="btn btn-secondary ml-2"
-            onClick={(e) => this.clear(e)}>
-                Cancelar</button>
+            <button
+              className="btn btn-secondary ml-2"
+              onClick={(e) => this.clear(e)}
+            >
+              Cancelar
+            </button>
           </div>
         </div>
       </div>
     );
   }
 
+  load(user) {
+    this.setState({ user });
+  }
+
+  remove(user) {
+    axios.delete(`${baseUrl}/${user.id}`).then((resp) => {
+      const list = this.getUpdatedList(user, false);
+      this.setState({ list });
+    });
+  }
+
+  renderTable() {
+    return (
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>{this.renderRows()}</tbody>
+      </table>
+    );
+  }
+
+  renderRows() {
+    return this.state.list.map((user) => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>
+            <button className="btn btn-warning" onClick={() => this.load(user)}>
+              <i className="fa fa-pencil"></i>
+            </button>
+            <button
+              className="btn btn-danger ml-2"
+              onClick={() => this.remove(user)}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
-    return <Main {...headerProps}>
+    return (
+      <Main {...headerProps}>
         {this.renderForm()}
-    </Main>;
+        {this.renderTable()}
+      </Main>
+    );
   }
 }
